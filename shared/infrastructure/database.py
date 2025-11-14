00c66929d3f295f309cdecfc8c2d1dc1c562e5a1
Al3xiel <gabriel280104casas@gmail.com>
2025-11-14 11:38:33 -0500

    chore: add iam.

---
+++ shared/infrastructure/database.py
@@ -0,0 +1,19 @@
+"""
+Database initialization and connection management for the Smart Band application.
+"""
+from peewee import SqliteDatabase
+
+# Initialize the SQLite database
+db = SqliteDatabase('bykerz_iot.db')
+
+def init_db()->None:
+    """
+    Initialize the database and create tables if they do not exist.
+
+    """
+    db.connect()
+    from iam.infrastructure.models import Device
+    from health.infrastructure.models import HealthRecord
+    db.create_tables([Device, HealthRecord], safe=True)
+    db.close()
+
